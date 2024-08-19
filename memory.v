fcd703cf0af37d899286b5c79159dfaf37e2dfbb
MatthewKing2 <matthewjesseking@gmail.com>
2024-08-19 17:01:19 -0500

    Added Memeory Modules (havent tested)

---
+++ memory.v
@@ -0,0 +1,55 @@
+
+//------------------------------------------------------------------------------
+// Module: memory
+// Description: A parameterizable block RAM module for FPGA designs. This module 
+//              implements a read/write memory with configurable address bus size,
+//              number of elements, and element size. It supports initialization 
+//              from a file and handles read and write operations based on control signals.
+//------------------------------------------------------------------------------
+
+module memory #( 
+    parameter       INIT_FILE   = "",   // Default to NULL (initalizes no values into memory)
+    parameter       AddrBusSize = 9,    
+    parameter       NumElements = 512,  
+    parameter       ElementSize = 8)(   
+    input   wire                            i_CLK,
+    // Enable Singals
+    input   wire                            i_write_en,
+    input   wire                            i_read_en,
+    // Addresses
+    input   wire    [AddrBusSize-1: 0]      i_write_addr,
+    input   wire    [AddrBusSize-1: 0]      i_read_addr,
+    // Data
+    input   wire    [ElementSize-1: 0]      i_write_data,
+    output  reg                             o_Ready_Bit, // Set when sucessfuly read / written
+    output  reg     [ElementSize-1: 0]      o_read_data);
+
+    //------------------------------------------------------------------------------
+    // Memory Declaration
+    //------------------------------------------------------------------------------
+    reg [ElementSize-1: 0] memory [NumElements];
+
+    //------------------------------------------------------------------------------
+    // Read/Write Operations
+    //------------------------------------------------------------------------------
+    always @(posedge i_CLK) begin
+        if(i_write_en) begin 
+            memory[i_write_addr] <= i_write_data;
+            o_Ready_Bit <= 1'b1; 
+        end
+        if(i_read_en) begin 
+            o_read_data <= memory[i_read_addr];
+            o_Ready_Bit <= 1'b1; 
+        end
+        if(~i_read_en && ~i_write_en)
+            o_Ready_Bit <= 1'b0; 
+    end
+
+    //------------------------------------------------------------------------------
+    // Memory Initialization
+    //------------------------------------------------------------------------------
+    initial if (INIT_FILE) begin
+        $readmemb(INIT_FILE, memory);
+    end
+
+endmodule
---
+++ memory_control.v
@@ -0,0 +1,67 @@
+
+module memory_control (
+    input wire i_CLK, 
+
+    // Control Signals
+    input wire          i_LD_MDR,
+    input wire          i_LD_MAR,
+    input wire          i_RW,
+    input wire          i_MIO_EN,
+
+    // From Datapath: 
+    input wire  [15:0]  i_Bus,
+
+    // Output 
+    output wire [15:0]  o_Bus,
+    output wire         o_Ready_Bit,
+    );
+
+    // Output of memeory wire
+    wire [15:0] w_Memory_Out;
+
+    // MDR and MAR Registers
+    // ---------------------------
+    reg [15:0] r_MDR;
+    reg [15:0] r_MAR;
+    assign o_Bus = r_MDR; // Output MDR onto CPU bus (gated by top mod)
+
+    always @(posedge i_CLK) begin
+        if(i_LD_MDR)
+            r_MDR <= w_Feed_MDR;
+        if(i_LD_MAR)
+            r_MAR <= i_Bus;
+    end
+    // ---------------------------
+
+
+    // Bus or Memory Mux (to MDR)
+    wire [15:0] w_Feed_MDR;
+    assign w_Feed_MDR = (i_MIO_EN) ? w_Memory_Out : i_Bus;
+
+
+    // Address Control Logic
+    // Uses MIO and RW to know when to read / write what
+    // ------------------------------------
+    wire w_Write = (i_MIO_EN && i_RW) ? 1'b1 : 1'b0;
+    wire w_Read = (i_MIO_EN && ~i_RW) ? 1'b1 : 1'b0;
+
+
+    // Init Sub Module 
+    memory #(
+        .INIT_FILE(""),
+        .AddrBusSize(16),    
+        .NumElements(512),  
+        .ElementSize(16)
+        ) Memory(   
+        .i_CLK(i_CLK),
+        .i_write_en(w_Write),
+        .i_read_en(w_Read),
+        .i_write_addr(r_MAR),
+        .i_read_addr(r_MAR),
+        .i_write_data(r_MDR),
+        .o_Ready_Bit(o_Ready_Bit)
+        .o_read_data(w_Memory_Out)
+    );
+
+
+endmodule
